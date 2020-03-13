073365602e95036cbc0811f018ec0f9df16ed896
jorgeteruya <40158063+jorgeteruya@users.noreply.github.com>
2020-03-12 23:02:05 -0300

    Projeto de criptografia em c#

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CRYTO_1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Limpar = new System.Windows.Forms.Button();
+            this.criptografar = new System.Windows.Forms.Button();
+            this.descriptografar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleDescription = "teste";
+            this.textBox1.AccessibleName = "teste";
+            this.textBox1.Location = new System.Drawing.Point(86, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(408, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 73);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(408, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(86, 118);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(408, 20);
+            this.textBox3.TabIndex = 0;
+            // 
+            // Limpar
+            // 
+            this.Limpar.Location = new System.Drawing.Point(211, 230);
+            this.Limpar.Name = "Limpar";
+            this.Limpar.Size = new System.Drawing.Size(172, 23);
+            this.Limpar.TabIndex = 1;
+            this.Limpar.Text = "Limpar";
+            this.Limpar.UseVisualStyleBackColor = true;
+            this.Limpar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // criptografar
+            // 
+            this.criptografar.Location = new System.Drawing.Point(211, 259);
+            this.criptografar.Name = "criptografar";
+            this.criptografar.Size = new System.Drawing.Size(172, 23);
+            this.criptografar.TabIndex = 1;
+            this.criptografar.Text = "criptografar";
+            this.criptografar.UseVisualStyleBackColor = true;
+            this.criptografar.Click += new System.EventHandler(this.criptografar_Click);
+            // 
+            // descriptografar
+            // 
+            this.descriptografar.Location = new System.Drawing.Point(211, 288);
+            this.descriptografar.Name = "descriptografar";
+            this.descriptografar.Size = new System.Drawing.Size(172, 23);
+            this.descriptografar.TabIndex = 1;
+            this.descriptografar.Text = "descriptografar";
+            this.descriptografar.UseVisualStyleBackColor = true;
+            this.descriptografar.Click += new System.EventHandler(this.descriptografar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Digite a baixo o texto a ser Criptografado:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Texto encriptado:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Texto descriptografado:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 386);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.descriptografar);
+            this.Controls.Add(this.criptografar);
+            this.Controls.Add(this.Limpar);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Limpar;
+        private System.Windows.Forms.Button criptografar;
+        private System.Windows.Forms.Button descriptografar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRYTO_1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+        }
+
+        private void criptografar_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = string.Empty;
+            for (int i=0; i < textBox1.Text.Length; i++)
+            {
+                int textoUsuario = (int)textBox1.Text[i];
+                int textoCripto = textoUsuario + 255;
+                textBox2.Text += Char.ConvertFromUtf32(textoCripto);
+            }
+        }
+
+        private void descriptografar_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = string.Empty;
+            for (int i = 0; i < textBox2.Text.Length; i++)
+            {
+                int textoCripto = (int)textBox2.Text[i];
+                int textoDecripto = textoCripto - 255;
+                textBox3.Text += Char.ConvertFromUtf32(textoDecripto);
+            }
+        }
+    }
+}
